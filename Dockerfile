cc84a14c3bd945e3f06b13648fce66d45d2a81b0
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2024-04-26 17:27:00 -0300

    adicionado rota de autenticacao com retorno de dados do usuario

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM node:18-alpine
 
 WORKDIR /usr/app
 
+ENV JWT_SECRET=ebb779d2-5418-405b-9e63-d0e603f08a98
+
+ENV DATABASE_URL="postgresql://postgres:LLEiaVeo6rSG@35.231.110.34:5432/some-postgres?schema=public"
+
 COPY package*.json ./
 
 RUN npm install 
--- src/http/controllers/auth/routes.ts
+++ src/http/controllers/auth/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../../middlewares/auth";
+import { getAuthUser } from ".";
 
 export async function authRoutes(app: FastifyInstance) {
 
   app.get('/auth', verifyJWT)
+
+  app.get('/auth-user', getAuthUser)
 
 }
