e9ce8e69f247c818063a99d5d33dca6a06f0d9e9
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2024-05-10 13:11:41 -0300

    nova variavel ambiente no dockerfile

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import { app } from "./app";
 import { env } from "./env";
 
 app.listen({
+  host: '0.0.0.0',
   port: env.PORT,
-}).then(() => {
+}).then((host) => {
+  console.log(host)
   console.log('HTTP Server Running!')
 })
